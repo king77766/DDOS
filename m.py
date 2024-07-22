9c147adb945a080b1632bd3e3fe90ef9fcb85ab5
king77766 <ngttally@gmail.com>
2024-07-22 11:14:31 +0530

    Update m.py

--- m.py
+++ m.py
@@ -8,10 +8,10 @@ import datetime
 import os
 
 # insert your Telegram bot token here
-bot = telebot.TeleBot('6985771977:AAHs45lBA9nZ93YsOjhr0LPxjsb6UMctdsg')
+bot = telebot.TeleBot('7364918975:AAHNmQxhYNGg5GJCqLCTrPNlK9-LTX3dSnw')
 
 # Admin user IDs
-admin_id = ["6159360725"]
+admin_id = ["677484790"]
 
 # File to store allowed user IDs
 USER_FILE = "users.txt"
@@ -220,7 +220,7 @@ COOLDOWN_TIME =0
 @bot.message_handler(commands=['bgmi'])
 def handle_bgmi(message):
     user_id = str(message.chat.id)
-    if user_id in allowed_user_ids:
+    if user_id not in allowed_user_ids:
         # Check if the user is in admin_id (admins have no cooldown)
         if user_id not in admin_id:
             # Check if the user has run the command before and is still within the cooldown period
